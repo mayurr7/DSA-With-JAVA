3bf748c4842a43ef8204ca49f832f624b9d8f247
Mayur Tekale <147367560+mayurr7@users.noreply.github.com>
2024-03-15 18:39:08 +0530

    Add files via upload

---
+++ sumOfRangeArray.java
@@ -0,0 +1,52 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class sumOfRangeArray {
+    public static int[] makePrefixSum(int[] arr){
+         for(int i = 1; i< arr.length; i++){
+            arr[i] += arr[i-1];
+        }
+        return arr;
+    }
+    
+    static void printArray(int arr[]){
+        for(int i =1; i< arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+       
+        System.out.print("Enter size of array=");
+         int n = sc.nextInt();
+        int arr[] = new int[n];
+       
+        System.out.println("enter element of array");
+         for(int j=1; j<arr.length; j++){
+            arr[j] = sc.nextInt();
+        }
+        System.out.print("Array is =");
+       printArray(arr);
+
+        int[] prefixSum = makePrefixSum(arr);
+
+       System.out.println("Enter number of queries = ");
+       int q=sc.nextInt();
+
+      
+
+       while (q-- > 0) {
+        System.out.println("Enter range : ");
+        int l = sc.nextInt();
+        int r = sc.nextInt();
+        int ans = prefixSum[r] - prefixSum[l-1];
+
+        System.out.println("Sum = " +ans);
+       }
+        
+
+    }
+}
